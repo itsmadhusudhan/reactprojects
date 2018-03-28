afa79c7999665db22dabc16b3bb5ce482ebfe68b
R <madhusudhan.r@atos.net>
2018-03-28 18:09:05 +0530

    added the todo master basic layout and functionality

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import TodoLayout from "./components/TodoLayout";
+
+
+ReactDOM.render(<TodoLayout/>, document.getElementById("root"));
